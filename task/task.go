cab5d87f97a75d03a73b20528bb991bc35af778d
rifat.zaman <rifat.zaman@upaybd.com>
2024-12-10 15:03:16 +0600

    update: Addtask version 2 for db

--- task/task.go
+++ task/task.go
@@ -2,6 +2,8 @@ package task
 
 import (
 	"fmt"
+	"gomind/db"
+	"log"
 	"time"
 )
 
@@ -10,6 +12,26 @@ type Task struct {
 	Description string
 	Due         time.Time
 	Completed   bool
+}
+
+func GetAllTasks() ([]Task, error) {
+	rows, err := db.DB.Query("SELECT id, description, due, completed FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.ID, &task.Description, &task.Due, &task.Completed); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return tasks, nil
 }
 
 type TaskManager struct {
@@ -23,6 +45,7 @@ func NewTaskManager() *TaskManager {
 
 func (tm *TaskManager) AddTask(description string, due time.Time) Task {
 	fmt.Println("Adding task:", description, due)
+
 	task := Task{
 		ID:          tm.NextID,
 		Description: description,
@@ -32,6 +55,21 @@ func (tm *TaskManager) AddTask(description string, due time.Time) Task {
 	tm.NextID++
 	tm.Tasks = append(tm.Tasks, task)
 	return task
+}
+
+func AddTaskv2(description string, due time.Time) (Task, error) {
+	var task Task
+	err := db.DB.QueryRow(
+		"INSERT INTO tasks (description, due) VALUES ($1, $2) RETURNING id",
+		description, due).Scan(&task.ID)
+	if err != nil {
+		return task, err
+	}
+
+	task.Description = description
+	task.Due = due
+	task.Completed = false
+	return task, nil
 }
 
 func (tm *TaskManager) ListTasks() []Task {
