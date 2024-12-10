06de030755e9da7eb90b78c1980504cce0e85181
rifat.zaman <rifat.zaman@upaybd.com>
2024-12-10 15:02:50 +0600

    update: db connection for

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+var DB *sql.DB
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func Connect() (*sql.DB, error) {
+	dbURL := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s port=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"))
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	DB = db
+	return db, nil
+}
